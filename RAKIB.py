cb7f4c12d7d22e4bd12fd691ecbd1f7da4e4a8b1
sanumod <99380568+sanumod@users.noreply.github.com>
2023-12-29 03:49:34 +0600

    Create RAKIB.py

---
+++ RAKIB.py
@@ -0,0 +1,6 @@
+import requests
+x1 = input("EMTER NUMBER:")
+x2 = int(input("ENTER LIMIT: "))
+for i in range(x2):
+api requests.post(f"https://siamrahman.xyz/sms_bkash.php?phone=+88{x1}").status_code
+....print(api)
